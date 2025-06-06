d3fe9ea413af7ed42dc9ed3842a3903e054e128d
Wooyoung Jang <wooyoung.jang@uta.edu>
2025-06-06 17:07:56 -0500

    experiment argument added for get_bearer_token() call

--- setup-dune-sl7.sh
+++ setup-dune-sl7.sh
@@ -9,7 +9,7 @@ run_command "Setting up dunesw $DUNELAR_VERSION $DUNELAR_QUALIFIER" setup dunesw
 echo "dunesw dir: $DUNESW_DIR"
 export DUNE_PLOT_STYLE_VERSION=v01_01
 run_command "Setting up dune_plot_style $DUNE_PLOT_STYLE_VERSION (null_qualifier)" setup dune_plot_style $DUNE_PLOT_STYLE_VERSION
-get_bearer_token
+get_bearer_token dune
 #export ROLE=Analysis
 #voms-proxy-init -rfc -noregen -voms=dune:/dune/Role=$ROLE -valid 120:00 -- deprecated, kx509 no longer being used
 setup ifdhc
