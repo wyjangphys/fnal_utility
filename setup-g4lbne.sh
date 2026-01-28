e6ba6e74b7e2c4c87715234551ed61ab7a49818b
Wooyoung Jang <wooyoung.jang@uta.edu>
2026-01-28 02:43:38 -0600

    g4lbne setup module

---
+++ setup-g4lbne.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /cvmfs/dune.opensciencegrid.org/dune-spack/spack-v0.23.0-fermi/BIWG/setup-env.sh
+spacktivate g4lbnf-geant4-10-4-3-gcc-12-2-0-cxx17-prof-almalinux9-x86_64_v2
+export G4LBNE_DIR=/exp/dune/app/users/wyjang/g4lbne
