604ae2dadb89ce93f13eea14721eafb5b7bef4a3
Wooyoung Jang <wooyoung.jang@uta.edu>
2026-02-19 11:55:01 -0600

    spack load command now uses hash values for root and ifdhc

--- setup-icarus-alma9.sh
+++ setup-icarus-alma9.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # Version table
-ROOT_VERSION=6.28.12
-GEANT_VERSION=10.6.1
+ROOT_VERSION=6.28.12;   ROOT_HASH="/sfwfmqo" # root@6.28.12%gcc@12.2.0 arch=linux-almalinux9-x86_64_v2
+GEANT_VERSION=10.6.1;   GEANT_HASH="/dkd5wst"
 CMAKE_VERSION=3.27.7
-IFDHC_VERSION=2.6.20
+IFDHC_VERSION=2.6.20;   IFDHC_HASH="/eu2izwm"
 GCC_VERSION=12.2.0
 FIFEUTILS_VERSION=3.7.4
 
@@ -13,9 +13,9 @@ echo "Running on Alma Linux 9"
 #. /cvmfs/larsoft.opensciencegrid.org/spack-packages/setup-env.sh # This is not work for xrootd protocol access!
 . /cvmfs/larsoft.opensciencegrid.org/spack-v0.22.0-fermi/setup-env.sh
 # check spack.readthedocs.io/en/latest/basic_usage.html for the detailed usage.
-run_command "Setting root $ROOT_VERSION via spack" spack load root@$ROOT_VERSION%gcc@12.2.0 arch=linux-almalinux9-x86_64_v3
+run_command "Setting root $ROOT_VERSION via spack" spack load $ROOT_HASH
 #run_command "Setting up grid access authorization (submit mode)" get_bearer_token icarus;export BEARER_TOKEN_FILE=/run/user/`id -u`/bt_u`id -u`
-run_command "Setting ifdhc via spack" spack load ifdhc@$IFDHC_VERSION
+run_command "Setting ifdhc via spack" spack load $IFDHC_HASH
 setup-g4
 set_prompt
 
